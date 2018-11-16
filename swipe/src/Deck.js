cb9bf0a588580aaf9ba728ac035e1c0178110f7f
cdnicoll <cdnicoll@gmail.com>
2018-11-16 12:39:30 -0800

    Used intropolation to rotate card

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -3,7 +3,10 @@ import {
     View,
     Animated,
     PanResponder,
+    Dimensions
 } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 class Deck extends React.Component {
     constructor(props) {
@@ -24,6 +27,19 @@ class Deck extends React.Component {
             position
         }
     }
+
+    getCardStyle() {
+        const { position } = this.state; // NOTE: Same as writing const position = this.state.position
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 2.0, 0, SCREEN_WIDTH * 2.0],
+            outputRange: ['-45deg', '0deg', '45deg']
+        })
+        return {
+            ...position.getLayout(), // spread operator (...) take all the different properites and add all the properties to the return
+            transform:[{ rotate }]
+        };
+    }
+
     /**
      * Loops through each data object and calls the renderCard method which
      * was passed in via props
@@ -34,7 +50,7 @@ class Deck extends React.Component {
                 return (
                     <Animated.View
                         key={item.id}
-                        style={this.state.position.getLayout()}
+                        style={this.getCardStyle()}
                         {...this.state.panResponder.panHandlers}
                     >
                         { this.props.renderCard(item) }
