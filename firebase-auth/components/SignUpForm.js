99316895efe0a7a07d16a652b7370b32ff7735b0
cdnicoll <cdnicoll@gmail.com>
2018-11-19 15:11:56 -0800

    Added some more code to the signup form, also added es2017 syntax for a little jazz

--- firebase-auth/components/SignUpForm.js
+++ firebase-auth/components/SignUpForm.js
@@ -10,12 +10,29 @@ import {
 } from 'react-native-elements';
 
 class SignupForm extends React.Component {
+    state = {
+        phone: ''
+    }
+
+    handleSubmit = () => {
+        console.log("ouch! you hit me! ... but here is the input " + this.state.phone)
+    }
+
+
     render() {
         return (
             <View>
-                <FormLabel>Enter Phone Number</FormLabel>
-                <FormInput />
-                <Button title="submit" />
+                <View style={{ marginBottom: 10 }}>
+                    <FormLabel>Enter Phone Number</FormLabel>
+                    <FormInput
+                        value={this.state.phone}
+                        onChangeText={phone => this.setState({ phone })}
+                    />
+                    <Button
+                        title="Submit"
+                        onPress={ this.handleSubmit }
+                    />
+                </View>
             </View>
         );
     }
